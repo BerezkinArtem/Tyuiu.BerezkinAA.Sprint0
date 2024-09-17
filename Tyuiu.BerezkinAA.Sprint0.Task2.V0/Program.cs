e5573bf9adba0c35d9ff8933afca4863c723427b
BerezkinArtem <berezkin.artem2006@gmail.com>
2024-09-18 01:22:07 +0500

    Task2

---
+++ Tyuiu.BerezkinAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Tyuiu.BerezkinAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BerezkinAA.Sprint0.Task2.V0
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Артем"));
+            Console.ReadKey();
+
+        }
+
+    }
+
+
+
+
+
+
+}
